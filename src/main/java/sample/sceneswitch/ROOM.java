a21057419f5d9f6cb717d379fd2c6c58afc75e9d
AHmed <ahmedgamal>
2024-12-03 22:31:07 +0200

    room

---
+++ src/main/java/sample/sceneswitch/ROOM.java
@@ -0,0 +1,20 @@
+package sample.sceneswitch;
+
+public abstract class ROOM {
+    protected String room_name;
+    protected int room_Id;
+    protected int numofvisitors;
+    protected int num_of_rooms;
+    protected Visitor[] List_of_Visitors;
+    protected Slot[] List_of_Slots;
+    public Room(String room_name, int room_Id, int numofvisitors, int num_of_rooms, Visitor[] List_of_Visitors, Slot[] List_of_Slots) {
+        this.room_name = room_name;
+        this.room_Id = room_Id;
+        this.numofvisitors = numofvisitors;
+        this.num_of_rooms = num_of_rooms;
+        this.List_of_Visitors = List_of_Visitors;
+        this.List_of_Slots = List_of_Slots;
+    }
+
+
+}
