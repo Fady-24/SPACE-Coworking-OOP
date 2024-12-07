cf90461f64bd9147d79b0afd633e1cbbf3c2d8e8
Fady-24 <u2864233@uel.ac.uk>
2024-12-08 01:27:09 +0200

    Visitor Homepage added

--- src/main/java/sample/sceneswitch/VisitorProfileController.java
+++ src/main/java/sample/sceneswitch/VisitorProfileController.java
@@ -3,53 +3,61 @@ package sample.sceneswitch;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class VisitorProfileController implements Initializable {
+
     @FXML
-    Button profileButton, ReservationButton, logout;
+    Label welcome,visitortypelabel,visitortotalhourslabel,balance;
     @FXML
-    Label type22,welcome,usernamelabel,passwordlabel,visitortypelabel,visitoridlabel,visitortotalhourslabel;
+    Rectangle rect;
     @FXML
-    RadioButton passtoggle;
+    ImageView reservations,icon;
 
     Visitor v = LoginController.Currentvisitor;
 
-    public void user_logout(ActionEvent e) throws IOException {
+    public void user_logout(MouseEvent e) throws IOException {
         HelloApplication h = new HelloApplication();
         h.changescene("starting.fxml");
     }
-//    public void prof_hover(MouseEvent e) throws IOException {
-//
-//        Animation.fade_transition((Button) e.getSource() );
-//
-//    }
-    public void show_pass(ActionEvent e) {
-        if (passtoggle.isSelected()) {
-            passwordlabel.setText("PASSWORD: " + v.getPassword());
-        }
-        else {
-            passwordlabel.setText("PASSWORD: " + v.getPassword().replaceAll(".","●"));
-        }
+    public void hover_in(MouseEvent e){
+        Animation.enlarge_image((Node)e.getSource());
     }
+    public void hover_out(MouseEvent e){
+        Animation.ensmall_image(((Node)e.getSource()));
+    }
+    public void button_transition(MouseEvent e) {
+        Animation.colorfillin((Shape) e.getSource(), Color.rgb(56, 56, 56), Color.rgb(249, 178, 51));
+    }
+    public void button_transition2(MouseEvent e) {
+        Animation.colorfillout((Shape) e.getSource(), Color.rgb(249, 178, 51), Color.rgb(56, 56, 56));
+    }
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        usernamelabel.setText("USERNAME: "+v.getName());
-        passwordlabel.setText("PASSWORD: " + v.getPassword().replaceAll(".","●"));
-        visitortypelabel.setText("VISITOR TYPE: "+v.getType());
-        visitoridlabel.setText("VISITOR ID: "+v.getID());
-        visitortotalhourslabel.setText("TOTAL HOURS: "+v.getHours());
-        System.out.println(v.getType());
-        type22.setText(v.getType());
-        welcome.setText("WELCOME, " + v.getName().toUpperCase().split(" ")[0]);
+        visitortypelabel.setText(v.getType());
+        if (v.getType().equals("INSTRUCTOR")){
+            Animation.changeImage(icon,"Asset 58.png");
 
+        }else if(v.getType().equals("FORMAL")){
+            Animation.changeImage(icon,"Asset 61.png");
+        }
+        visitortotalhourslabel.setText(String.valueOf(v.getHours()));
+        welcome.setText(v.getName().toUpperCase().split(" ")[0]);
+        balance.setText(String.valueOf(v.getBalance())+" $");
+        System.out.println(v.getType());
     }
 }
